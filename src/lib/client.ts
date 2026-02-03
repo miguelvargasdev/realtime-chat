4657c2549b92303d0439f677691434ccde5b85da
Miguel Vargas <contact@miguelvargas.dev>
2026-02-03 13:09:12 -0500

    Refactor client initialization to use environment variable for production URL

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,9 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
+const domain = process.env.NEXT_PUBLIC_API_URL || "localhost:3000";
+const url = domain.startsWith("http") ? domain : `https://${domain}`;
+
 export const client = treaty<App>(
-	process.env.NEXT_PUBLIC_API_URL || "localhost:3000",
+	process.env.NODE_ENV === "development" ? "http://localhost:3000" : url,
 ).api;
